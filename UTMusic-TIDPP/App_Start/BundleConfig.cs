5a610a91ef63e4a8e71aa9e3d5e6a1a26b5fa637
killerart <442234@mail.ru>
2020-10-20 17:29:26 +0300

    packages

---
+++ UTMusic-TIDPP/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace UTMusic_TIDPP
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/Content/css/all.min.css",
+                "~/Content/mdbootstrap/css/bootstrap.min.css",
+                "~/Content/mdbootstrap/css/mdb.min.css",
+                "~/Content/mdbootstrap/css/style.css",
+                "~/Content/plyr.css",
+                "~/Content/main.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include("~/Scripts/jquery-3.4.1.min.js",
+                "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                "~/Content/mdbootstrap/js/popper.min.js",
+                "~/Content/mdbootstrap/js/bootstrap.min.js",
+                "~/Content/mdbootstrap/js/mdb.min.js",
+                "~/Scripts/html5media.min.js",
+                "~/Scripts/plyr.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include(
+                    "~/Scripts/jquery.validate.min.js",
+                    "~/Scripts/jquery.validate.unobtrusive.min.js"
+                )
+            );
+            bundles.Add(new ScriptBundle("~/bundles/myPlayer/js").Include("~/Scripts/myPlayer.js"));
+            bundles.Add(new ScriptBundle("~/bundles/myPlayerInit/js").Include("~/Scripts/myPlayerInit.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/jquery-3.4.1.min.js"));
+        }
+    }
+}
--- UTMusic-TIDPP/Global.asax.cs
+++ UTMusic-TIDPP/Global.asax.cs
@@ -1,23 +1,32 @@
-﻿using System;
+﻿using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
-using System.Web.Http;
+using UTMusic.BusinessLogic.Infrastructure;
+using UTMusic.Web.Util;
 
-namespace UTMusic_TIDPP
+namespace UTMusic_TIDPP.Web
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
-        {
+        void Application_Start(object sender, EventArgs e) {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //NinjectModule dependencyModule = new DependencyModule();
+            //NinjectModule serviceModule = new ServiceModule("MusicContext");
+            //var kernel = new StandardKernel(dependencyModule, serviceModule);
+            //DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }
     }
 }
